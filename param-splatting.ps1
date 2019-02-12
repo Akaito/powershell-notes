656509c4a52eef2abe5d69d8a46256c0e1eba5c4
Chris Barrett <chris@codesaru.com>
2019-02-12 11:32:11 -0800

    More comments on splatting.  Don't use @args.
    "args" turns out to be a special variable name.

--- param-splatting.ps1
+++ param-splatting.ps1
@@ -1,13 +1,15 @@
 # Splatting makes it easy to pass a collection of arguments to a command's params.
 # PS' true/false values are variables or variable-like, hence the `$true` below.
 
-$args = @{
-    Name = "explorer";
+# Key-value pair lines can end in semicolons if you prefer.
+# Can't add param names like "Banana" that don't exist in the receiving function.
+$parameters = @{
+    Name = "explorer"
     WhatIf = $true
 }
 
 # Making the following line into `Write-Host $args` would just print the type.
-$args
+$parameters
 
-Stop-Process @args
+Stop-Process @parameters
 
